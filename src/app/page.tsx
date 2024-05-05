8661dd7f6fd1ad6f987f1da572cb7c8ff8e2d449
“Warley <warleycoutinho@icloud.com>
2024-05-05 15:58:02 -0300

    criando o header e adicionando animações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import SettingsTabs from "@components/SettingsTabs";
+
 const Home = () => {
-  return <h1>Hello Tailwindcss</h1>;
+  return <>
+  <h1 className="text-3xl font-medium text-zinc-900">Hello Tailwindcss</h1>
+  <SettingsTabs/>
+  </>
 };
 
 export default Home;
