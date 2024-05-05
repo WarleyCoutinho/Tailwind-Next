e20cf1549c56ccbea1982ebdccf84ab6ebf6fc66
“Warley <warleycoutinho@icloud.com>
2024-05-05 20:46:37 -0300

    criando a estrutura do form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ const config: Config = {
   theme: {
     extend: {
       gridTemplateColumns:{
-        app:  'minmax(18rem, 20rem) 1fr',
-        profile:'max-content 1fr min-content',
+        app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr max-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
       maxWidth:{
         app:'700px'
